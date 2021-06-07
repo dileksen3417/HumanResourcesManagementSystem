24d16cfd5a349d446460a2f805d988a962b3c026
dsen <dileksen3417@gmail.com>
2021-06-07 21:30:30 +0300

    Swagger added

--- src/main/java/com/senSoftware/HumanResourceManagement/HumanResourceManagementApplication.java
+++ src/main/java/com/senSoftware/HumanResourceManagement/HumanResourceManagementApplication.java
@@ -2,12 +2,24 @@ package com.senSoftware.HumanResourceManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class HumanResourceManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HumanResourceManagementApplication.class, args);
 	}
-
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.senSoftware.HumanResourceManagement"))
+				.build();
+	}
 }
